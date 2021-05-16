ff72168c9b36462e5291dba4ad7ebbf100a80f97
Manisha <manisha10850@gmail.com git config --global useer.email manisha10850@gmail.comgit config --global user.email manisha10850@gmail.com>
2021-05-16 16:52:00 +0530

    test runner class

--- src/test/java/runnerpackage/TestRunner.java
+++ src/test/java/runnerpackage/TestRunner.java
@@ -5,11 +5,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/main/java/connectresources/81124.feature", "src/main/java/connectresources/78740.feature"},
+@CucumberOptions(features = {"src/main/java/resource/Homepage.feature"},
 
 
-                               plugin = {"pretty","html:target/cucmber-html-report",
-                            		   "json:target/cucucmber.json","junit:target/cucumber.xml"}, glue = {"test"})
+                               plugin = {"pretty","html:target/cucmber-html-report"}, glue = {"test"})
 
 
 public class TestRunner
